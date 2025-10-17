8551b77bdca1dbe66ecc7449ac34a635d07f7e16
Jan Greń <jaxyt550@gmail.com>
2025-10-17 23:52:02 +0200

    fixed recipe crafting

--- Assets/Scripts/Recipe.cs
+++ Assets/Scripts/Recipe.cs
@@ -9,6 +9,7 @@ public class Recipe : MonoBehaviour, IPointerClickHandler
     [SerializeField] public GameObject result;
     [SerializeField] public int resultQuantity;
     [SerializeField] public float craftingTime = 0.5f;
+    private Dictionary<int, int> ingredientsDict;
     private InventoryController inventoryController;
     private int ingredientsCount;
     private TMP_Text quantityText;
@@ -17,46 +18,66 @@ public class Recipe : MonoBehaviour, IPointerClickHandler
     {
         inventoryController = InventoryController.Instance;
         
+        ingredientsDict = new Dictionary<int, int>();
         quantityText = GetComponentInChildren<TMP_Text>();
         result.GetComponent<Item>().quantity = resultQuantity;
         quantityText.text = resultQuantity.ToString();
-        ingredientsCount = ingredients.Count;
+
+        SetItemDictionary();
     }
     
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (eventData.button == PointerEventData.InputButton.Left)
+        if (eventData.button != PointerEventData.InputButton.Left) return;
+        
+        var invItems = inventoryController.GetInventorySaveItems();
+
+        for (int i = 0; i < invItems.Count; i++)
         {
-            var invItems = inventoryController.GetInventorySaveItems();
-
-            for (int i = 0; i < invItems.Count; i++)
+            if (ingredientsDict.ContainsKey(invItems[i].itemID))
             {
-                foreach (GameObject ingredient in ingredients)
-                {
-                    if (invItems[i].itemID == ingredient.GetComponent<Item>().ID)
-                    {
-                        ingredientsCount--;
-                    }
-                }
-            }
-
-            if (ingredientsCount == 0)
-            {
-                inventoryController.AddItem(result);
-                Debug.Log("Recipe finished.");
-                Debug.Log("Result quantity: " + result.GetComponent<Item>().quantity);
-                Debug.Log("Result quantity 2: " + resultQuantity);
-                ingredientsCount = ingredients.Count;
-
-                foreach (GameObject ingredient in ingredients)
-                {
-                    inventoryController.RemoveItem(ingredient);
-                }
-            }
-            else
-            {
-                Debug.Log("Not enough items.");
+                ingredientsDict[invItems[i].itemID] -= invItems[i].quantity;
             }
         }
+
+        foreach (var i in ingredientsDict)
+        {
+            if (i.Value <= 0)
+            {
+                ingredientsCount--;
+            }
+        }
+            
+        if (ingredientsCount == 0)
+        {
+            inventoryController.AddItem(result);
+            Debug.Log("Recipe finished.");
+            SetItemDictionary();
+
+            foreach (GameObject ingredient in ingredients)
+            {
+                inventoryController.RemoveItem(ingredient);
+            }
+        }
+        else
+        {
+            Debug.Log("Not enough items.");
+        }
+    }
+    
+    private void SetItemDictionary()
+    {
+        ingredientsDict.Clear();
+        foreach (GameObject ingredient in ingredients)
+        {
+            var currItem = ingredient.GetComponent<Item>();
+            
+            if (!ingredientsDict.TryAdd(currItem.ID, 1))
+            {
+                ingredientsDict[currItem.ID]++;
+            }
+        }
+        
+        ingredientsCount = ingredientsDict.Count;
     }
 }
